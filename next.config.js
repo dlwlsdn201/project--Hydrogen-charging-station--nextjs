ad5514a1fe9fd0556b7a7561a6da6bf9c965deff
JinWoo_Lee <dlwlsdn201@naver.com>
2023-10-06 21:27:16 +0900

    :wrench: build bundle 최적화를 위한 webpack optimization 초기 설정

--- next.config.js
+++ next.config.js
@@ -1,27 +1,32 @@
 /** @type {import('next').NextConfig} */
-
-const TerserPlugin = require('terser-webpack-plugin');
-
+const isProduction = process.env.NODE_ENV === "production";
+const TerserPlugin = require("terser-webpack-plugin");
 const nextConfig = {
-   webpack: (
-    config,
-    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
-  ) => {
-    // Important: return the modified config
+  webpack: (config) => {
+    // 기존 대비 약 50% bundle file size 감소 효과
+    config.optimization = {
+      ...config.optimization,
+      minimize: isProduction, // minimizer 에 설정된 플러그인을 기반으로 번들을 최소화 시킴.
+      minimizer: isProduction
+        ? [
+            new TerserPlugin({
+              // test: /\.(js|jsx|ts|tsx)$/,
+              parallel: true,
+              terserOptions: {
+                format: {
+                  comments: false, // 빌드 시, comment 제거 (주석 제거)
+                },
+                compress: {
+                  drop_console: true, // 빌드 시, console.* 구문 코드 제거
+                },
+              },
+              extractComments: false, // 주석을 별도의 파일로 추출할 지 여부
+            }),
+          ]
+        : [],
+    };
+    return config;
+  },
+};
 
-    optimization: {
-      minimize: false; // minimizer 에 설정된 플러그인을 기반으로 번들을 최소화 시킴.
-      minimizer: [
-      new TerserPlugin({
-        parallel: true,
-        terserOptions: {
-          // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
-        },
-      }),
-    ];
-    }
-    return config
-  }
-}
-
-module.exports = nextConfig
+module.exports = nextConfig;
