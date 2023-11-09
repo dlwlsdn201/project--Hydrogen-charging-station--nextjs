cd439809efd77fc5ec683ffd24d27dcdcbaa40ee
JinWoo_Lee <dlwlsdn201@naver.com>
2023-11-09 23:21:58 +0900

    :construction::sparkles: 수소차 등록 현황 차트 데이터 Type 정의

---
+++ app/api/dashboard.ts
@@ -0,0 +1,15 @@
+import { BASE_URL } from '@app/static/api';
+
+const apiKey = 'KOmVWhRTLJbltSjetYWBfguGDSkRw3U2mROGLgYPMO896f1xSChfUfTIo%2BLAI1uW1uRA8AiGUZE%2Fo4Os3uXBbw%3D%3D';
+
+export const READ_REG_STATUS_DATA = async () => {
+  const res = await fetch(
+    `${BASE_URL}/15117132/v1/uddi:29120ccb-cd91-4436-b5b7-ecdac6d5dc35?page=1&perPage=10&serviceKey=${apiKey}&_type=json`,
+    {
+      method: 'GET',
+      // mode: 'cors',
+      // next: { revalidate: 10 },
+    },
+  );
+  return res;
+};
--- types/dashboard/chart.d.ts
+++ types/dashboard/chart.d.ts
@@ -1,3 +1,12 @@
+export type TRegRegionLabels = '강원' | '경기' | '경남' | '경북' | '광주' | '대구' | '대전' | '부산' | '서울' | '세종';
+export interface IRegDataObj {
+  승용: number;
+  승합: number;
+  화물: number;
+  총합계: number;
+  지역: TRegRegionLabels;
+}
+
 export interface IDatasetKeys {
   label: string;
   data: number[];
